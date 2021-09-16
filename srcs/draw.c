39fda348516b8d02de715588128cc4325d6ebaae
yavuzsonmez <contact@yavuzsonmez.com>
2021-09-16 14:41:31 +0200

    split

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 15:10:50 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/09/16 14:20:54 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/09/16 14:40:51 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,108 +24,82 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 /* Draw line between points with Bresenham algorithm */
 
-void draw_line (t_data *img, t_matrix *matrix, t_parse *data, t_screen *screen, t_matrix *isomatrix)
+
+static void bresenham (int x0, int x1, int y0, int y1, int color, t_screen *screen, t_data *img)
 {
-	size_t i;
+
+		int sx;
+		int sy;
+
+		int dx =  abs (x1 - x0);
+		if (x0 < x1)
+			sx = 1;
+		else
+			sx = -1;
+		int dy = -abs (y1 - y0);
+		if (y0 < y1)
+			sy = 1;
+		else
+			sy = -1;
+		int err = dx + dy;
+		int e2;
+		while (1)
+		{
+
+			if (x0 >= 0 && x0 < screen->SCREEN_W && y0 >= 0 && y0 < screen->SCREEN_H)
+				my_mlx_pixel_put(img, x0, y0, color);
+			if (x0 == x1 && y0 == y1)
+				break;
+			e2 = 2 * err;
+			if (e2 >= dy)
+			{
+				err += dy;
+				x0 += sx;
+			}
+			if (e2 <= dx)
+			{
+				err += dx;
+				y0 += sy;
+			}
+		}
+}
+
+
+link_x
+
+link_y
+
+void draw(t_data *img, t_matrix *matrix, t_parse *data, t_screen *screen, t_matrix *isomatrix)
+{
+	size_t	i;
+	int		x0;
+	int		x1;
+	int		y0;
+	int		y1;
+	int color;
 
 	i = 0;
 	while (i < data->size - 1)
 	{
 		if (matrix[i].y != matrix[i + 1].y)
 			i++;
-		int x1 = isomatrix[i + 1].x;
-		int x0 = isomatrix[i].x;
-		int y1 = isomatrix[i + 1].y;
-		int y0 = isomatrix[i].y;
-		int sx;
-		int sy;
-
-		int dx =  abs (x1 - x0);
-		if (x0 < x1)
-			sx = 1;
-		else
-			sx = -1;
-		int dy = -abs (y1 - y0);
-		if (y0 < y1)
-			sy = 1;
-		else
-			sy = -1;
-		int err = dx + dy;
-		int e2;
-		while (1)
-		{
-			if (x0 >= 0 && x0 < screen->SCREEN_W && y0 >= 0 && y0 < screen->SCREEN_H)
-				my_mlx_pixel_put(img, x0, y0, isomatrix[i].color);
-			if (x0 == x1 && y0 == y1)
-				break;
-			e2 = 2 * err;
-			if (e2 >= dy)
-			{
-				err += dy;
-				x0 += sx;
-			}
-			if (e2 <= dx)
-			{
-				err += dx;
-				y0 += sy;
-			}
-		}
+		x0 = isomatrix[i].x;
+		x1 = isomatrix[i + 1].x;
+		y0 = isomatrix[i].y;
+		y1 = isomatrix[i + 1].y;
+		color = isomatrix[i].color;
+		bresenham (x0, x1, y0, y1, color, screen, img);
 		i++;
 	}
-}
-
-void draw_col (t_data *img, t_matrix *matrix, t_parse *data, t_screen *screen, t_matrix *isomatrix)
-{
-	(void)matrix;
-	size_t i;
-
 	i = 0;
 	while (i < (data->size - data->col))
 	{
-		int x1 = isomatrix[i + data->col].x;
-		int x0 = isomatrix[i].x;
-		int y1 = isomatrix[i + data->col].y;
-		int y0 = isomatrix[i].y;
-		int sx;
-		int sy;
-
-		int dx =  abs (x1 - x0);
-		if (x0 < x1)
-			sx = 1;
-		else
-			sx = -1;
-		int dy = -abs (y1 - y0);
-		if (y0 < y1)
-			sy = 1;
-		else
-			sy = -1;
-		int err = dx + dy;
-		int e2;
-		while (1)
-		{
-
-			if (x0 >= 0 && x0 < screen->SCREEN_W && y0 >= 0 && y0 < screen->SCREEN_H)
-				my_mlx_pixel_put(img, x0, y0, isomatrix[i].color);
-			if (x0 == x1 && y0 == y1)
-				break;
-			e2 = 2 * err;
-			if (e2 >= dy)
-			{
-				err += dy;
-				x0 += sx;
-			}
-			if (e2 <= dx)
-			{
-				err += dx;
-				y0 += sy;
-			}
-		}
+		x0 = isomatrix[i].x;
+		x1 = isomatrix[i + data->col].x;
+		y0 = isomatrix[i].y;
+		y1 = isomatrix[i + data->col].y;
+		color = isomatrix[i].color;
+		bresenham (x0, x1, y0, y1, color, screen, img);
 		i++;
 	}
-}
-
-void draw(t_data *img, t_matrix *matrix, t_parse *data, t_screen *screen, t_matrix *isomatrix)
-{
-	draw_line (img, matrix, data, screen, isomatrix);
-	draw_col (img, matrix, data, screen, isomatrix);
 }
