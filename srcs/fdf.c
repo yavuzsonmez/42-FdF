0dcfc9dc3ebba8426efe083c0d029be3285b38e5
yavuzsonmez <contact@yavuzsonmez.com>
2021-10-22 14:33:45 +0200

    finish

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -12,6 +12,22 @@
 
 #include "../includes/FdF.h"
 
+/*	Screen starting data initialisation */
+
+static void	init_screen_data(char **argv, t_fdf *fdf)
+{
+	fdf->screen->file = argv[1] + 10;
+	fdf->screen->scale = 10;
+	fdf->screen->translate_x = (WIDTH / 2) - ((fdf->data.col / 2)
+			* fdf->screen->scale);
+	fdf->screen->translate_y = (HEIGHT / 2) - ((fdf->data.row / 2)
+			* fdf->screen->scale);
+	fdf->screen->alt = 0;
+	fdf->screen->alpha = 0.9;
+	fdf->screen->beta = -0.8;
+	fdf->screen->theta = 0.2;
+}
+
 /*	Render Routine:
 *	Create the view of the graphic or re-render it after an event hook
 */
@@ -39,31 +55,18 @@ void	render(t_fdf *fdf)
 }
 
 /*	Window creation
-*	Screen  starting data initialisation
 *	First rendering
 */
 
 static void	create_window(char **argv, t_fdf *fdf)
 {
 	fdf->screen = (t_screen *)ft_calloc(sizeof(t_screen), 1);
-	fdf->screen->file = argv[1] + 10;
-	fdf->screen->scale = 10;
-	fdf->screen->translate_x = (WIDTH / 2) - ((fdf->data.col / 2)
-			* fdf->screen->scale);
-	fdf->screen->translate_y = (HEIGHT / 2) - ((fdf->data.row / 2)
-			* fdf->screen->scale);
-	fdf->screen->alt = 0;
-	fdf->screen->alpha = 0.9;
-	fdf->screen->beta = -0.8;
-	fdf->screen->theta = 0.2;
+	fdf->isomatrix = (t_matrix *)ft_calloc(sizeof(t_matrix), fdf->data.size);
+	if (fdf->screen == NULL || fdf->isomatrix == NULL)
+		return ;
+	init_screen_data(argv, fdf);
 	fdf->vars.mlx = mlx_init();
 	fdf->vars.win = mlx_new_window(fdf->vars.mlx, WIDTH, HEIGHT, "FdF");
-	fdf->isomatrix = (t_matrix *)ft_calloc(sizeof(t_matrix), fdf->data.size);
-	if (fdf->isomatrix == NULL)
-	{
-		free_data(fdf);
-		return ;
-	}
 	render(fdf);
 	mlx_hook(fdf->vars.win, 2, 1L << 0, key_handler, fdf);
 	mlx_hook(fdf->vars.win, 4, 1L << 2, zoom, fdf);
